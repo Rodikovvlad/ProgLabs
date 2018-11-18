7527e53616de20cbe566c5f3f0adba772414268a
Vlad <39596326+Rodikovvlad@users.noreply.github.com>
2018-11-19 06:20:53 +0700

    Update Lab5-2.cpp

--- Lab5-2/Lab5-2.cpp
+++ Lab5-2/Lab5-2.cpp
@@ -1,1 +1,27 @@
-erf
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL,"Russian");
+    string s, result;
+    int num=0;
+    cout << "Введите строку:";
+    cin >> s;
+    for(int i = 0; s[i] != NULL; i++) {
+        if (s[i] != '.' && std::isdigit(s[i]) || s[0] == '-')  {
+            result += s[i];
+            num++;
+        }
+        else {
+            cout << "Не является";
+            num=0;
+            break;
+        }
+    }
+    if (num != 0 ) cout << result;
+
+    return 0;
+}
+
