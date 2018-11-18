e070b0baaa2b53428758f4fe84616d16d05721d0
Vlad <39596326+Rodikovvlad@users.noreply.github.com>
2018-11-19 06:23:37 +0700

    Update Lab6-2.cpp

--- Lab6-2/Lab6-2.cpp
+++ Lab6-2/Lab6-2.cpp
@@ -1,1 +1,43 @@
-rtgbbfv
+#include <iostream>
+#define N 4
+#include <cmath>
+
+using namespace std;
+
+void lab(float x[N], float y[N], float z[N]) {
+
+	float mult1 = 1;
+	float mult2 = 1;
+
+	for (int i = 0; i < N; i++) {
+		if (1 - (y[i] * y[i]) > 0.5) {
+			mult1 *= sin(x[i]) + 2;
+		}
+		else
+		{
+			mult2 *= 1 - (z[i] * z[i]);
+		}
+	}
+
+	cout << mult1 << endl;
+	cout << mult2 << endl;
+}
+
+
+
+int main()
+{	
+	setlocale(LC_ALL, "Russian");
+	float x[N], y[N], z[N];
+
+	cout << "Заполните массивы" << endl;
+	
+	cin >> x[0] >> x[1] >> x[2] >> x[3];
+	cout << endl;
+	cin >> y[0] >> y[1] >> y[2] >> y[3];
+	cout << endl;
+	cin >> z[0] >> z[1] >> z[2] >> z[3];
+	
+	lab(x, y, z);
+}
+
