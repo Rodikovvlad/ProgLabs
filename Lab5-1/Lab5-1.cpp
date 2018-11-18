a365a365bb9aef740b8e3378e3a96499e52b2c99
Vlad <39596326+Rodikovvlad@users.noreply.github.com>
2018-11-19 06:19:34 +0700

    Update Lab5-1.cpp

--- Lab5-1/Lab5-1.cpp
+++ Lab5-1/Lab5-1.cpp
@@ -1,1 +1,28 @@
-dv
+include <iostream>
+#include <stdlib.h>
+#include <sstream>
+#include <cstring>
+#include <string>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    string s,ch;
+    int num = 0;
+    stringstream p;
+    cout << "Введите строку: ";
+    cin >> s;
+
+
+    for (int i = 1; i < 10; i++) {
+        for (int k = 0; s[k] != NULL; k++) {
+            char sym = i + '0';
+            if (s[k] == sym) num++;
+        }
+        cout << "Кол-во "<<i<<": "<<num<<endl;
+        num=0;
+    }
+    return 0;
+}
+
