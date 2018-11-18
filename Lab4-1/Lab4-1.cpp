a7f9896392407e98d01ba940cd2f59f9c2ba9115
Vlad <39596326+Rodikovvlad@users.noreply.github.com>
2018-11-19 06:15:43 +0700

    Update Lab4-1.cpp

--- Lab4-1/Lab4-1.cpp
+++ Lab4-1/Lab4-1.cpp
@@ -1,1 +1,39 @@
-hey
+#include <iostream>
+#include <ctime>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL,"Russian");
+    srand(time(0));
+    int countMin = 0, number = 0, r;
+    cout << "Введите кол-во элементов: ";
+    cin >> r;
+    const int els = r;
+    int x[els];
+    for (int i = 0; i < els; i++) {
+        x[i] = rand() % 200 - 100;
+    }
+    int y[els];
+    for (int i = 0; i < els; i++) {
+        if (x[i] > 0) number += 1;
+    }
+    for (int i = 0; i < els; i++) {
+        if (x[i] < 0) {
+            y[countMin] = x[i];
+            countMin++;
+        }
+        else if (x[i] > 0) {
+            y[els - number - 1] = x[i];
+            number--;
+        }
+    }
+
+    for ( int i = 0; i < els; i++) {
+        cout << y[i] << " ";
+    }
+    system("pause");
+    return 0;
+}
